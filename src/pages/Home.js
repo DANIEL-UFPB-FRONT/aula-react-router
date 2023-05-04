4369d58e87ecc4bb7b4a5bf480a51a4a7c79c41f
Daniel Faustino <@daniel>
2023-05-04 14:28:00 -0300

    Adding examples to home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,13 @@ function Home() {
     return (
         <div>
             <h1>Home</h1>
+            <p>React router dom example. Please click on links below or try:</p>
+            <ul>
+                <li>Route /contact/id (ex: /contact/1 ) to see dynamic routing</li>
+                <li>Any other route diferent from list below (ex: /any) to see not found routing</li>
+            </ul>
+
+            <hr></hr>
             <nav>
                 <ul>
                     <li><Link to="/">Home</Link></li>
